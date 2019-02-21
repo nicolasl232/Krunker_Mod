e1acb441db667e08820d3fd25c31b6a3bf81e0b8
nl <nicolasl232@riiotnicolas@gmail.com>
2019-02-21 16:10:22 -0500

    Beautifier

---
+++ HTML_KrunkerBeautifier.js
@@ -0,0 +1,18 @@
+
+var ammoIcon = document.getElementById("ammoIcon");
+ammoIcon.src = "http://img4.wikia.nocookie.net/__cb20130711173219/teamfortress/images/b/b0/Ammo_icon_TF2.png";
+
+var inputChat = document.getElementById("chatInput");
+inputChat.style.borderRadius = "10px";
+
+var SkullIcon = document.getElementById("killsIcon");
+SkullIcon.src = "http://cdn.onlinewebfonts.com/svg/img_493161.svg";
+
+var HealthValue = document.getElementById("healthValue");
+HealthValue.style.padding = "9px";
+var HealthBarH = document.getElementById("healthBarH");
+HealthBarH.style.borderRadius = "10px";
+var healthBar = document.getElementById("healthBar");
+healthBarH.style.borderRadius = "10px";
+
+
